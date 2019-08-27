e157922af6ed8dc2982ff7bda1434e251bd26f51
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-08-27 16:33:58 -0400

    Add project files.

---
+++ StudentClassProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StudentClassProject {
+    class Program {
+        static void Main(string[] args) {
+            //Setting properties have to put (name of variable.property = Script)
+
+            var Lidya = new Student();
+            Lidya.FirstName = "lidya";
+            Lidya.LastName = "Tsegay";
+            Lidya.SetFinalGrade(98);
+            var FullName = Lidya.GetFullName();
+            Console.WriteLine("Lidya's Full Name Is " + FullName);
+
+            var Justin = new Student();
+            Justin.FirstName = "Justin";
+            Justin.LastName = "Dudley";
+            Justin.SetFinalGrade(99);
+            FullName = Justin.GetFullName();
+            Console.WriteLine($"{FullName}");
+
+            var Nick = new Student();
+            Nick.FirstName = "Nick";
+            Nick.LastName = "Baker";
+            Nick.SetFinalGrade(99);
+                      Console.WriteLine($"{Nick.FirstName} {Nick.LastName}");
+        }
+        
+    }
+}
---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassProject {
+
+
+    class Student {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int FinalGrade { get; set; }
+    
+        public string GetFullName() {
+
+            return FirstName + " " + LastName;
+        }
+        public void SetFinalGrade(int finalGrade) {
+            if (finalGrade < 0)
+            {
+                Console.WriteLine("ERROR: grade must be greater than zero");
+
+            }
+            else
+            {
+                FinalGrade = finalGrade;
+            }
+        }
+    }
+
+
+}
